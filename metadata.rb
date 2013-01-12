a4a38ca4febe8a839b67fb452dcd4731c5bb4080
Chris Roberts <chrisroberts.code@gmail.com>
2013-01-12 08:40:55 -0800

    Bump version 0.1.0

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer_email 'chrisroberts.code@gmail.com'
 license 'Apache 2.0'
 description 'Provides managed hosts file'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.0.1'
+version '0.1.0'
 
