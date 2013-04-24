064d93c3ae339ee845bf4f0f79e59896ff07048b
Pavel Druzyak <pavel.druzyak@gmail.com>
2013-04-25 00:37:36 +0300

    Add localhost_aliases

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
 default[:hosts_file][:path] = '/etc/hosts'
 default[:hosts_file][:define_self] = 'ip_address' # or 'loopback' or 'localhost_only'
+default[:hosts_file][:localhost_aliases] = []
 default[:hosts_file][:custom_entries] = {}
