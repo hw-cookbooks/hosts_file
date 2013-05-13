646c31ab012c15e9332a4d1cefd470531adb8abd
Chris Roberts <chrisroberts.code@gmail.com>
2013-05-13 08:29:59 -0700

    Update version for release 0.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'chrisroberts.code@gmail.com'
 license 'Apache 2.0'
 description 'Provides managed hosts file'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.1.2'
 
