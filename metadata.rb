76742be8bc7720393abf1ea6af774215c78dab45
Aaron Baer <aaron@hw-ops.com>
2016-03-25 14:43:53 -0700

    contact information and source_url issue_url

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,9 @@
 name	'hosts_file'
-maintainer 'Chris Roberts'
-maintainer_email 'chrisroberts.code@gmail.com'
+maintainer 'Heavy Water Operations, LLC.'
+maintainer_email 'helpdesk@heavywater.io'
 license 'Apache 2.0'
 description 'Provides managed hosts file'
+source_url        "https://github.com/hw-cookbooks/hosts_file" if respond_to?(:source_url)
+issues_url        "https://github.com/hw-cookbooks/hosts_file/issues" if respond_to?(:issues_url)
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.2.1'
